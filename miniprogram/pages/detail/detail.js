db9b4c5994f03981d8794f5e4d3b9181b6102685
eidopal <jonnysocial@gmail.com>
2026-05-22 19:03:10 +0800

    Add save-to-album, share, and publish guidance to detail page

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -1,6 +1,15 @@
 const api = require('../../utils/api')
 
 Page({
+  onShareAppMessage: function () {
+    var images = this.data.result.visual_assets.processed_images || []
+    return {
+      title: 'BIKON 生成的水印图片',
+      path: '/pages/detail/detail?task_id=' + this.data.taskId,
+      imageUrl: images[0] || ''
+    }
+  },
+
   data: {
     taskId: '',
     taskStatus: '',
@@ -66,9 +75,100 @@ Page({
 
   previewImage: function (e) {
     var url = e.currentTarget.dataset.url
+    var allUrls = this.data.result.visual_assets.processed_images || [url]
     wx.previewImage({
-      urls: [url],
+      urls: allUrls,
       current: url
+    })
+  },
+
+  saveImage: function (e) {
+    var url = e.currentTarget.dataset.url
+    wx.showLoading({ title: '保存中...' })
+    wx.downloadFile({
+      url: url,
+      success: function (res) {
+        wx.saveImageToPhotosAlbum({
+          filePath: res.tempFilePath,
+          success: function () {
+            wx.hideLoading()
+            wx.showToast({ title: '已保存到相册', icon: 'success' })
+          },
+          fail: function () {
+            wx.hideLoading()
+            wx.showToast({ title: '请授权相册权限', icon: 'none' })
+          }
+        })
+      },
+      fail: function () {
+        wx.hideLoading()
+        wx.showToast({ title: '下载失败，请重试', icon: 'none' })
+      }
+    })
+  },
+
+  saveAllImages: function () {
+    var urls = this.data.result.visual_assets.processed_images || []
+    if (!urls.length) return
+    var that = this
+    wx.showLoading({ title: '保存中 1/' + urls.length })
+
+    function saveNext(index) {
+      if (index >= urls.length) {
+        wx.hideLoading()
+        wx.showToast({ title: '全部已保存到相册', icon: 'success' })
+        return
+      }
+      wx.showLoading({ title: '保存中 ' + (index + 1) + '/' + urls.length })
+      wx.downloadFile({
+        url: urls[index],
+        success: function (res) {
+          wx.saveImageToPhotosAlbum({
+            filePath: res.tempFilePath,
+            success: function () { saveNext(index + 1) },
+            fail: function () {
+              wx.showToast({ title: '请授权相册权限', icon: 'none' })
+              wx.hideLoading()
+            }
+          })
+        },
+        fail: function () {
+          saveNext(index + 1)
+        }
+      })
+    }
+    saveNext(0)
+  },
+
+  copyAllAndGo: function (e) {
+    var platform = e.currentTarget.dataset.platform
+    var result = this.data.result.copywriting || {}
+    var text = ''
+
+    if (platform === 'xhs' && result.xiaohongshu) {
+      text = result.xiaohongshu.text
+      if (result.xiaohongshu.tags) {
+        text += '\n\n' + result.xiaohongshu.tags.join(' ')
+      }
+    } else if (platform === 'wechat' && result.wechat_moments) {
+      text = result.wechat_moments.text
+    }
+
+    if (!text) {
+      wx.showToast({ title: '暂无可复制文案', icon: 'none' })
+      return
+    }
+
+    wx.setClipboardData({
+      data: text,
+      success: function () {
+        wx.showModal({
+          title: '文案已复制',
+          content: platform === 'xhs' ? '已复制小红书文案，去小红书 App 发布吧' : '已复制朋友圈文案，去微信发布吧',
+          showCancel: false,
+          confirmText: '好的'
+        })
+      }
     })
   }
 })
